dbfcadced6d76f21d2e50d74b148e395e70929db
mkamilmistar <kamil_mistar@apps.ipb.ac.id>
2019-04-29 17:29:16 +0700

    Update home.blade.php

--- resources/views/sites/home.blade.php
+++ resources/views/sites/home.blade.php
@@ -16,57 +16,6 @@
 </section>
 <!-- ##### Hero Area End ##### -->
 
-<!-- ##### Register Now Start ##### -->
-<section class="register-now section-padding-100-0 d-flex justify-content-between align-items-center" style="background-image: url({{asset('/sites')}}/img/core-img/texture.png);">
-    <!-- Register Contact Form -->
-    <div class="register-contact-form mb-100 wow fadeInUp" data-wow-delay="250ms">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="forms">
-                        <h4>Courses For Free</h4>
-                        <form action="#" method="post">
-                            <div class="row">
-                                <div class="col-12 col-lg-6">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" id="text" placeholder="Name">
-                                    </div>
-                                </div>
-                                <div class="col-12 col-lg-6">
-                                    <div class="form-group">
-                                        <input type="email" class="form-control" id="email" placeholder="Email">
-                                    </div>
-                                </div>
-                                <div class="col-12 col-lg-6">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" id="phone" placeholder="Phone">
-                                    </div>
-                                </div>
-                                <div class="col-12 col-lg-6">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" id="site" placeholder="Site">
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <button class="btn clever-btn w-100">Send Message</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Register Now Countdown -->
-    <div class="register-now-countdown mb-100 wow fadeInUp" data-wow-delay="500ms">
-        <h3>Daftar Sekarang!</h3>
-        <p>Dengan mendaftar anda sudah membantu kucing yang belum dapat bini, ayo tunggu apalagi, mpus juga punya rasa punya hati</p>
-        <!-- Register Countdown -->
-        <div class="register-countdown">
-            <div class="events-cd d-flex flex-wrap" data-countdown="2019/03/01"></div>
-        </div>
-    </div>
 </section>
 <!-- ##### Register Now End ##### -->
 
