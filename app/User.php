46680bed4fdf15d4d3ecd3e0d11029bf291b27bf
Tengkusandraa <sandra_tengku08@apps.ipb.ac.id>
2019-05-09 15:24:53 +0700

    iye

--- app/User.php
+++ app/User.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Authenticatable  implements MustVerifyEmail
 {
     use Notifiable;
 
