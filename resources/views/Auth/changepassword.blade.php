3193e20274a5cfbf41b66438a3541dd7ca7d0be2
mkamilmistar <kamil_mistar@apps.ipb.ac.id>
2019-05-13 10:17:45 +0700

    fix blade change password

--- resources/views/Auth/changepassword.blade.php
+++ resources/views/Auth/changepassword.blade.php
@@ -1,71 +1,67 @@
-@extends('layouts.app')
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        @if(session('gagal'))
-        <div class="alert alert-icon alert-danger alert-dismissible fade in" role="alert">
-            <button type="button" class="close" data-dissmis="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            <i class="mdi mdi-check-all"></i>
-            <strong>Oh Snap!</strong>{{session('gagal')}}
-        </div>
-        @endif
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('savepassword') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="oldpassword" class="col-md-4 col-form-label text-md-right">{{ __('Old Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="oldpassword" type="password" class="form-control @error('oldpassword') is-invalid @enderror" name="oldpassword" required autocomplete="oldpassword" autofocus>
-
-                                @error('oldpassword')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Update') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@extends('Auth.layouts.master')
+@section('title')
+RuntahWeb | Register
 @endsection
+@section('content')
+
+<body class="hold-transition register-page">
+    <div class="register-box">
+        <div class="register-logo">
+            <a href="/"><b>Runtah</b>Web</a>
+        </div>
+
+        <div class="register-box-body">
+            <p class="login-box-msg">Register a new volunteer </p>
+
+            <form method="POST" action="{{ route('savepassword') }}">
+                @csrf
+
+                <div class="form-group has-feedback">
+                    <input placeholder="Password Lama" id="oldpassword" type="password" class="form-control @error('oldpassword') is-invalid @enderror" name="oldpassword" required autocomplete="oldpassword" autofocus>
+                    <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+                    @error('oldpassword')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="form-group has-feedback">
+                    <input placeholder="Password Baru" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                    <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="form-group has-feedback">
+                    <input placeholder="Confirmasi Password" id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                    <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+                </div>
+
+                <div class="form-group row mb-0">
+                    <div class="col-md-6 offset-md-4">
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Update') }}
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+            <a href="/login" class="text-center">I already have a membership</a>
+        </div>
+        <!-- /.form-box -->
+    </div>
+    <!-- /.register-box -->
+
+    <script>
+        $(function() {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' /* optional */
+            });
+        });
+    </script>
+    @endsection
